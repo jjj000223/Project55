fddfddd2f57123136ad25b76d7e242c0ec00e6f6
지연 이 <jjj000223@daum.net>
2019-12-10 13:10:38 +0900

    초기 커밋.

---
+++ Project55/1210.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int data[] = { 3,21,35,57,24,82,8 };
+	int* pdata = data;
+	int i;
+	for (i = 0; i < 7; i++)
+	{
+		*(data + i) += 10;
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
